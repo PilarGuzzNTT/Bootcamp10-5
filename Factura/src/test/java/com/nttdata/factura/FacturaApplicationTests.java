100df8827e61f1076a5f742ebdde725a33f0dc70
PilarGuzzNTT <130760774+PilarGuzzNTT@users.noreply.github.com>
2023-05-10 12:44:55 +0200

    Practica microServicios

---
+++ Factura/src/test/java/com/nttdata/factura/FacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Inventario/src/main/java/com/nttdata/inventario/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.nttdata.inventario.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/obtener")
+	public String inventario() {
+		return "inventario";
+	}
+	
+	
+
+}
